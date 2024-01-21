27cc95dfb6048e7ee527a2e981389d10a61127d5
et1083 <et1083@gmail.com>
2024-01-21 07:52:02 -0800

    moves one charting routine and brings in chart container script

--- jscript/ham_simple.js
+++ jscript/ham_simple.js
@@ -1952,24 +1952,6 @@ function show_test_results_resp(){
     return totunpassed;
   }
 
-function get_pie_chart(unseenpassed, total, x, y){
-    var piedata = [];
-    var pie_url = [];
-    piedata[0] = unseenpassed;
-    piedata[1] = total - unseenpassed;
-    pie_url.push(' <img src="http://chart.apis.google.com/chart?chs=' + x + 'x' + y);
-    //chart_url.push(score_colors);
-    pie_url.push('&amp;chco=FF0000|00FF00');  
-    //pie_url.push('&amp;chl=Untested|Tested');
-    pie_url.push('&amp;chd=');
-    pie_url.push(simpleEncode(piedata, total));  
-    pie_url.push('&amp;cht=p');  
-    pie_url.push('">');
-    //pie_url.push('&amp;chtt= Question Pool Completion' + '">'); 
-    //alert(pie_url.join(''));
-    return pie_url.join('');  
-
-}
 
 function insert_graph( graph_scores, title, colors){
   //Encapsulate this as the progress charting method
